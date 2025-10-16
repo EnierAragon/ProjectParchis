a2479b39ed93fb9c8a72d50a279cc85233ebe3c5
EnierAragon <91.enier.91@gmail.com>
2025-10-16 11:02:07 -0600

    First commit

---
+++ src/main/java/cr/ac/ucr/projectparchis/ProjectParchis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ucr.projectparchis;
+
+/**
+ *
+ * @author enier
+ */
+public class ProjectParchis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
