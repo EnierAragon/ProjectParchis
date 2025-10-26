42f933475d9712145d4fcb9926d4d3c5872090ac
EnierAragon <91.enier.91@gmail.com>
2025-10-26 10:18:24 -0600

    Se agrego el javadoc

--- src/main/java/cr/ac/ucr/projectparchis/model/Box.java
+++ src/main/java/cr/ac/ucr/projectparchis/model/Box.java
@@ -7,6 +7,8 @@ package cr.ac.ucr.projectparchis.model;
 /**
  *
  * @author enier
+ * @version 1.0
+ * @since 1.0
  */
 public class Box {
 
@@ -14,36 +16,47 @@ public class Box {
      * Cordenada de la esquina superior izquierda de la casilla Las cordenadas
      * siguen el orden X,Y,Z.
      *
+     * @since 1.0
      */
     private double[] boxPoint1 = new double[2];
 
     /**
      * Cordenada de la esquina inferior derecha de la casilla Las cordenadas
      * siguen el orden X,Y,Z.
+     *
+     * @since 1.0
      */
     private double[] boxPoint2 = new double[2];
 
     /**
      * Cordenada de el centro de la casilla Las cordenadas siguen el orden
      * X,Y,Z.
+     *
+     * @since 1.0
      */
     private double[] midCord = new double[2];
 
     /**
      * Cordenada de la seccion izquieda de la casilla Las cordenadas siguen el
      * orden X,Y,Z.
+     *
+     * @since 1.0
      */
     private double[] leftCord = new double[2];
 
     /**
      * Cordenada de la seccion derecha de la casilla Las cordenadas siguen el
      * orden X,Y,Z.
+     *
+     * @since 1.0
      */
     private double[] rightCord = new double[2];
 
     /**
      * Este Parametro determina si la casilla tiene que tener un comportamiento
      * Especial.
+     *
+     * @since 1.0
      */
     private boolean specialBox = false;
 
@@ -72,6 +85,14 @@ public class Box {
         calcRightPoint();
     }
 
+    /**
+     * Este metodo calcula el centro de los puntos de caja 1 y 2 con formula
+     * p2-+(p1 - p2)/2 este metodo puede calcular puntos negativos sin problemas
+     * almenos de forma. Esto se guarda en {@code midCord[]}
+     *
+     * @see midCord
+     * @since 1.0
+     */
     private void calcMid() {
 
         double distX = Math.abs(boxPoint1[0] - boxPoint2[0]);
@@ -91,6 +112,14 @@ public class Box {
 
     }
 
+    /**
+     * Este metodo calcula el punto medio entre punto de Caja 1 y Cordenada Mid
+     * manteniendo la posicion Y, con la misma formula que {@code calcMid()}.
+     * Este metodo guarda los datos en {@code leftCord[]}
+     *
+     * @see calcMid()
+     * @see leftCord
+     */
     private void calcLeftPoint() {
 
         double distX = Math.abs(boxPoint1[0] - midCord[0]);
@@ -105,6 +134,14 @@ public class Box {
 
     }
 
+    /**
+     * Este metodo calcula el punto medio entre Cordenada Mid y punto de Caja 2
+     * manteniendo la posicion Y, con la misma formula que {code calcMid()}.
+     * Este metodo guarda los datos en {@code rightCord[]}.
+     *
+     * @see calcMid()
+     * @see rightCord
+     */
     private void calcRightPoint() {
 
         double distX = Math.abs(midCord[0] - boxPoint2[0]);
@@ -119,14 +156,32 @@ public class Box {
 
     }
 
+    /**
+     * Metodo para conseguir la referencia de la cordenada superior izquierda de
+     * la casilla
+     *
+     * @return Referencia a el array primitivo double {@code boxPoint1}
+     * @see boxPoint1
+     */
     public double[] getBoxCord1() {
         return boxPoint1;
     }
 
+    /**
+     * Establece la cordenada superior izquierda **Este metodo es solo para
+     * pruebas**
+     *
+     * @param bCord1 referencia que copiara {@code boxPoint1}
+     * @see boxPoint1
+     */
     public void setBoxCord1(double[] bCord1) {
         this.boxPoint1 = bCord1;
     }
 
+    /**
+     * Metodo para conseguir la referencia de la cordenada inferior derecha del panel
+     * @return 
+     */
     public double[] getBoxCord2() {
         return boxPoint2;
     }
@@ -135,7 +190,7 @@ public class Box {
         this.boxPoint2 = bCord2;
     }
 
-    public double[] getSingleCord() {
+    public double[] getMidCord() {
         return midCord;
     }
 
