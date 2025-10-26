af07004724b3ad05b7dca23c5c6b90df7c13469e
EnierAragon <91.enier.91@gmail.com>
2025-10-26 14:07:09 -0600

    se agrego un actualizador del cuadrado este permite cambiar las cordenadas del mismo

--- src/main/java/cr/ac/ucr/projectparchis/view/test/PanelTest.java
+++ src/main/java/cr/ac/ucr/projectparchis/view/test/PanelTest.java
@@ -14,19 +14,23 @@ import javax.swing.JPanel;
  */
 public class PanelTest extends JPanel {
 
+    int[] xPoints = {50, 250, 250, 50};
+    int[] yPoints = {50, 50, 200, 200};
+    int nPoints = 4;
+
     public PanelTest() {
+    }
+
+    public void setCords(int[] cords1, int[] cords2) {
+        xPoints = cords1;
+        yPoints = cords2;
+        this.repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Coordenadas del polígono (un rectángulo)
-        int[] xPoints = {50, 250, 250, 50};
-        int[] yPoints = {50, 50, 200, 200};
-        int nPoints = 4;
-
-        // Configurar color y dibujar
         g.setColor(Color.RED);
         g.fillPolygon(xPoints, yPoints, nPoints);
     }
